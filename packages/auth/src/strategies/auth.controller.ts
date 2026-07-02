880510db960437179882a6ba94a79b3385afa5a5
TreborJs <rob132ca@gmail.com>
2026-07-02 16:20:23 -0400

    refactor(rbac): move Roles decorator to rbac/

--- packages/auth/src/strategies/auth.controller.ts
+++ packages/auth/src/strategies/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body, UseGuards, Request, HttpCode, HttpStatus } from
 import { AuthService } from '../services/auth.service';
 import { MagicLinkService } from '../services/magic-link.service';
 import { Public } from '../decorators/public.decorator';
-import { Roles } from '../decorators/roles.decorator';
+import { Roles } from '../rbac/roles.decorator';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { RolesGuard } from '../rbac/roles.guard';
 import {
